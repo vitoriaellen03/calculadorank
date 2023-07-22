73a2f1c257241329fb3e74527aaa59c33f3baeb6
Vitória Ellen <contato.vitoriaellen03@gmail.com>
2023-07-22 15:04:08 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -57,44 +57,6 @@
 // }
 
 
-/// Imprime a Imagem
-$(document).ready(function () {
-    $("#download").on("click", function () {
-        var element = document.getElementById("htmlContent");
-
-        // Define a largura e altura desejadas para a imagem em pixels
-        var canvasWidth = 5900;
-        var canvasHeight = 7000;
-
-        // Calcula a escala com base na largura desejada do canvas e a largura do elemento
-        var scale = canvasWidth / element.offsetWidth;
-
-        // Cria um novo canvas com a resolução desejada
-        var canvas = document.createElement("canvas");
-        canvas.width = canvasWidth;
-        canvas.height = canvasHeight;
-        var context = canvas.getContext("2d");
-
-        // Usa a biblioteca html2canvas para renderizar o conteúdo no canvas
-        html2canvas(element, { scale: scale })
-            .then(function (renderedCanvas) {
-                // Desenha a imagem renderizada no novo canvas
-                context.drawImage(renderedCanvas, 0, 0, canvasWidth, canvasHeight);
-
-                // Obtém a URL da imagem a partir do canvas
-                var imageData = canvas.toDataURL("image/jpeg", 1.0);
-
-                // Cria um link para baixar a imagem
-                var link = document.createElement("a");
-                link.setAttribute("download", "image.jpg");
-                link.setAttribute("href", imageData);
-                link.click();
-            })
-            .catch(function (error) {
-                console.error("Erro ao renderizar a imagem:", error);
-            });
-    });
-});
 
 /// Mascara de Dinheiro
 $(function () {
